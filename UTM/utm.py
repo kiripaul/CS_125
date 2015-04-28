5ac8413a19334391eb177d53bb5363fb506a87fb
Kiripaul <kiripaul@gmail.com>
2015-04-28 12:31:06 -0400

    Refining inputs for TM

--- UTM/utm.py
+++ UTM/utm.py
@@ -5,11 +5,12 @@
 
 ## Defining a TM class first so that it can be passed to the UTM
 class TM:
-	def __init__(self, states, input_alpha, tape_alpha, transition_func, start_state, blank_sym, finish_state):
-		self.states = {}
+	def __init__(self, states, tape_alpha, transition_func, finish_state):
+		self.states = states
 		self.input_alpha = [0,1]
-		self.tape_alpha = {}
-		self.transition_func = {}
+		self.tape_alpha = tape_alpha
+		self.transition_func = transition_func
 		self.start_state = 'q0' #in the definition of this TM and UTM, I will always use q0 as a start state
 		self.blank_sym = "/"
-		self.finish_state = ''
+		self.finish_state = finish_state
+
